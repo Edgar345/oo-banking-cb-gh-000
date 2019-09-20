0a5dbb0c284d2c2928088f35df0921b97e7f0d92
Edgar V <evenegas9@ucmerced.edu>
2019-09-20 06:09:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
   def reverse_transfer
     puts "STATUS: #{self.status}"
     #can only reverse completed transaction
-    if (self.status == "complete") 
+    if self.status == "complete"
       self.sender.deposit(self.amount)
       self.receiver.deposit(-self.amount)
       self.status = "reversed"
